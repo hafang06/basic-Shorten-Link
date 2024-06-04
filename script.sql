667d10806785a71e32049ac3cce7686188a450d0
Hoang Hai Phong <haiphong110806@gmail.com>
2024-06-04 21:56:57 +0700

    add new table

--- script.sql
+++ script.sql
@@ -9,3 +9,13 @@ INSERT INTO ShortenLinks (ShortLink, OriginalLink) VALUES ('uyendao2510', 'https
 SELECT OriginalLink FROM ShortenLinks WHERE ShortLink = 'uyendao2510';
 
 DELETE FROM ShortenLinks WHERE ShortLink = 'uyendao2510';
+
+-- create s1mple table
+
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    hire_date DATE
+);
